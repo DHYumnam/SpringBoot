7802fb1768062bb53e9e1b091d0760854262c669
DHYumnam <denimmei@gmail.com>
2023-09-07 11:20:40 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,5 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Welcome to Spring Boot! Dhiren!");
+		System.out.println("Pull request! Dhiren!");
 	}
 }
