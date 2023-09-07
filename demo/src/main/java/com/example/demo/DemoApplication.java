b27f3f5c0a7590c69b0d32205d017ce3bebe41f8
dhiren <denimmei@gmail.com>
2023-09-07 11:23:57 +0530

    test

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Welcome to Spring Boot! Dhiren!");
+
 		System.out.println("Pull request! Dhiren!");
+
 	}
 }
